0c38302478052f04a0d51f8b4a5d688eff4d5c77
Carlos Antônio da Silva <carlos.antonio@cardeal.corp>
2025-01-08 14:36:50 -0300

    Atualiza para a versão 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dcalendario",
-    version="0.1.0",
+    version="0.1.1",
     author="Carlos Antonio",
     author_email="carlos22martinssilva@gmail.com",
     description="Pacote para gerar uma tabela dCalendário com Python",
