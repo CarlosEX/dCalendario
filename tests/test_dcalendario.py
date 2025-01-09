b3fc5cba83796c9ed140b37e5779b9317af3e674
Carlos Antônio da Silva <carlos.antonio@cardeal.corp>
2025-01-09 11:32:51 -0300

    corrige erro da função de calendario nome da funcao

--- tests/test_dcalendario.py
+++ tests/test_dcalendario.py
@@ -1,9 +1,9 @@
 import unittest
-from dcalendario.dcalendario import criar_dcalendario
+from dcalendario.dcalendario import generate_calendar_df
 
 class TestDCalendario(unittest.TestCase):
     def test_criar_dcalendario(self):
-        calendario = criar_dcalendario('2024-01-01', '2024-12-31')
+        calendario = generate_calendar_df('2024-01-01', '2024-12-31')
         self.assertEqual(len(calendario), 366)  # Ano bissexto
         self.assertIn('AnoAtual', calendario.columns)
 
